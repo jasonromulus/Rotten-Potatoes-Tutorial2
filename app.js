d7453c150eeb6256575dbae3bd14b4c1d1026692
jasonromulus <pumcome@me.com>
2018-09-30 15:47:39 -0700

    Users can destory reviews

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const methodOverride = require('method-override')
+const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
@@ -8,12 +10,14 @@ const Review = mongoose.model('Review', {
   movieTitle: String
 });
 const bodyParser = require('body-parser');
+const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
-})
+// app.get('/', (req, res) => {
+//   res.render('home', { msg: 'Hello World!' });
+// })
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
@@ -49,7 +53,44 @@ app.get('/reviews/new', (req, res) => {
 // CREATE
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
-    console.log(review);
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  })
+})
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+// EDIT
+app.get('/reviews/:id/edit', (req, res) => {
+  Review.findById(req.params.id, function(err, review) {
+    res.render('reviews-edit', {review: review});
+  })
+})
+
+// UPDATE
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+// DELETE
+app.delete('/reviews/:id', function (req, res) {
+  console.log("DELETE review")
+  Review.findByIdAndRemove(req.params.id).then((review) => {
     res.redirect('/');
   }).catch((err) => {
     console.log(err.message);
