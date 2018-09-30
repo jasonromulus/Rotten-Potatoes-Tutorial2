364e2458fe9f961927ca3ddeb3a9c07db3124be6
jasonromulus <pumcome@me.com>
2018-09-30 01:18:13 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -13,3 +13,14 @@ var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
+
+// INDEX
+app.get('/', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
+})
